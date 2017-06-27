78ce9063466b8ec1ca7fd0bdf539c34edb7304cf
Egor Petrov <e.petrov@freematiq.com>
2017-06-27 16:16:14 +0700

    js

--- web/js/preview.js
+++ web/js/preview.js
@@ -1,6 +1,4 @@
 $(function() {
-    // Multiple images preview in browser
-
     $('#preview').click(function () {
         $('#preview_username').html($('#user_name').val());
         $('#preview_email').html($('#InputEmail').val());
@@ -12,33 +10,5 @@ $(function() {
         reader.onloadend = function () {
             preview.src = reader.result;
         }
-
-        if (file) {
-            reader.readAsDataURL(file);
-        } else {
-            preview.src = "";
-        }
-    });
-
-    var imagesPreview = function(input, placeToInsertImagePreview) {
-
-        if (input.files) {
-            var filesAmount = input.files.length;
-
-            for (i = 0; i < filesAmount; i++) {
-                var reader = new FileReader();
-
-                reader.onload = function(event) {
-                    $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
-                }
-
-                reader.readAsDataURL(input.files[i]);
-            }
-        }
-
-    };
-
-    $('#gallery-photo-add').on('change', function() {
-        imagesPreview(this, 'div.gallery');
     });
 });
